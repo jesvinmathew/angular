4b90bc91e204cdddceee8c2d19623f157306cf7d
jesvinmathew <jesvinmathew@gmail.com>
2023-02-26 22:55:55 +0530

    Click event, for and if

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myApp';
+  //array of months.
+  months = ["January", "Feburary", "March", "April",
+  "May", "June", "July", "August", "September",
+  "October", "November", "December"];
+  isavailable = true;
+  myClickFunction() {
+  //just added console.log which will display the event details in browser on click of the button.
+  alert("Button is clicked");
+  console.log();
+}
 }
